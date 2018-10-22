8cc2d8f400898434d32bdae6507bf806253bb33c
Ahmet Selçuk Özyurt <asozyurt@gmail.com>
2018-10-22 20:24:33 +0300

    Style changes, relative path fixes, 2 new blog post merged

--- simple-blog-engine-for-asp-net-core-developers/wwwroot/js/main/script.js
+++ simple-blog-engine-for-asp-net-core-developers/wwwroot/js/main/script.js
@@ -3,66 +3,23 @@
     // Document ready
 
     $(document).ready(function () {
-
-        // Sticky nav
-        //var header = $('.main-nav'),
-        ////    pos = header.offset();
-
-        //header.slideUp(200, function () {
-        //    $(this).removeClass('switched-header').addClass('default').fadeIn(200);
-        //});
-
+        
         // Section scroll 
         $('a.scroll').smoothScroll({
             speed: 800,
             offset: -64
         });
-        
-        ////Portfolio setup 
-
-        //$('.popup').magnificPopup({
-        //    type: 'image',
-        //    fixedContentPos: false,
-        //    fixedBgPos: false,
-        //    mainClass: 'mfp-no-margins mfp-with-zoom',
-        //    image: {
-        //        verticalFit: true
-        //    },
-        //    zoom: {
-        //        enabled: true,
-        //        duration: 300
-        //    }
-        //});
-
-        //var blockWorks = $('.block-works');
-        //$('.popup-youtube, .popup-vimeo').magnificPopup({
-        //    disableOn: 700,
-        //    type: 'iframe',
-        //    mainClass: 'mfp-fade',
-        //    removalDelay: 160,
-        //    preloader: false,
-        //    fixedContentPos: false
-        //});
-
-
-        //$('.filter ').on("click", "li a", function () {
-        //    $(this).addClass('active');
-        //    $(this).parent().siblings().find('a').removeClass('active');
-        //    var filters = $(this).attr('data-filter');
-        //    $(this).closest(blockWorks).find('.item').removeClass('disable');
-
-        //    if (filters !== 'all') {
-        //        var selected = $(this).closest(blockWorks).find('.item');
-        //        for (var i = 0; i < selected.length; i++) {
-        //            if (!selected.eq(i).hasClass(filters)) {
-        //                selected.eq(i).addClass('disable');
-        //            }
-        //        }
-        //    }
-
-        //    return false;
-        //});
-
+        $("#block-slider").owlCarousel({
+            slideSpeed: 300,
+            paginationSpeed: 400,
+            responsiveRefreshRate: 200,
+            responsiveBaseWidth: window,
+            pagination: false,
+            singleItem: true,
+            navigation: true,
+            navigationText: ["<span class='icon-left-open-big'></span>", "<span class='icon-right-open-big'></span>"]
+        });
+       
        
         // To the top handler
 
